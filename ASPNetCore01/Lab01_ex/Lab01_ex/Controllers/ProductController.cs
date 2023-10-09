68dbba545b06c2e6dafcd8d0210396529aee17b6
unknown <tranhuuhoang1407@gmail.com>
2023-10-09 09:56:11 +0700

    Day31

---
+++ ASPNetCore01/Lab01_ex/Lab01_ex/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab01_ex.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
