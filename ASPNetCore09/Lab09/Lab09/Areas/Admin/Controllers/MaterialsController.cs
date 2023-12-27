f91f84697250034f7cf329602a5712a1068a7f36
unknown <tranhuuhoang1407@gmail.com>
2023-12-27 15:11:41 +0700

    Day51

---
+++ ASPNetCore09/Lab09/Lab09/Areas/Admin/Controllers/MaterialsController.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Lab09.Models;
+using X.PagedList;
+using Newtonsoft.Json;
+
+namespace Lab09.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MaterialsController : BaseController
+    {
+        private readonly DevXuongMocContext _context;
+
+        public MaterialsController(DevXuongMocContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Materials
+        public async Task<IActionResult> Index(string name, int page = 1)
+        {
+            //số bản ghi trên 1 trang
+            int limit = 5;
+
+            var account = await _context.Materials.OrderBy(c => c.Id).ToPagedListAsync(page, limit);
+            if (!String.IsNullOrEmpty(name))
+            {
+                account = await _context.Materials.Where(c => c.Title.Contains(name)).OrderBy(c => c.Id).ToPagedListAsync(page, limit);
+            }
+            ViewBag.keyword = name;
+            return View(account);
+        }
+
+        // GET: Admin/Materials/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Materials == null)
+            {
+                return NotFound();
+            }
+
+            var material = await _context.Materials
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (material == null)
+            {
+                return NotFound();
+            }
+
+            return View(material);
+        }
+
+        // GET: Admin/Materials/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Materials/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Icon,MetaTitle,MetaKeyword,MetaDescription,Slug,Orders,Parentid,CreatedDate,UpdatedDate,AdminCreated,AdminUpdated,Notes,Status,Isdelete")] Material material)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count() > 0 && files[0].Length > 0)
+                {
+                    var file = files[0];
+                    var FileName = file.FileName;
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Content\\Uploads\\images\\material", FileName);
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                        material.Icon = "/Content/Uploads/images/material/" + FileName;
+                    }
+                }
+
+                material.CreatedDate = DateTime.Now;
+                material.UpdatedDate = DateTime.Now;
+                var admin = JsonConvert.DeserializeObject<AdminUser>(HttpContext.Session.GetString("AdminLogin"));
+                material.AdminCreated = admin.Account;
+                material.AdminUpdated = admin.Account;
+
+                _context.Add(material);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(material);
+        }
+
+        // GET: Admin/Materials/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Materials == null)
+            {
+                return NotFound();
+            }
+
+            var material = await _context.Materials.FindAsync(id);
+            if (material == null)
+            {
+                return NotFound();
+            }
+            return View(material);
+        }
+
+        // POST: Admin/Materials/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Icon,MetaTitle,MetaKeyword,MetaDescription,Slug,Orders,Parentid,CreatedDate,UpdatedDate,AdminCreated,AdminUpdated,Notes,Status,Isdelete")] Material material)
+        {
+            if (id != material.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var files = HttpContext.Request.Form.Files;
+                    if (files.Count() > 0 && files[0].Length > 0)
+                    {
+                        var file = files[0];
+                        var FileName = file.FileName;
+                        var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Content\\Uploads\\images\\material", FileName);
+                        using (var stream = new FileStream(path, FileMode.Create))
+                        {
+                            file.CopyTo(stream);
+                            material.Icon = "/Content/Uploads/images/material/" + FileName;
+                        }
+                    }
+
+                    material.UpdatedDate = DateTime.Now;
+                    var admin = JsonConvert.DeserializeObject<AdminUser>(HttpContext.Session.GetString("AdminLogin"));
+                    material.AdminUpdated = admin.Account;
+
+                    _context.Update(material);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MaterialExists(material.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(material);
+        }
+
+        // GET: Admin/Materials/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            //if (id == null || _context.Materials == null)
+            //{
+            //    return NotFound();
+            //}
+
+            //var material = await _context.Materials
+            //    .FirstOrDefaultAsync(m => m.Id == id);
+            //if (material == null)
+            //{
+            //    return NotFound();
+            //}
+
+            //return View(material);
+
+            if (_context.Materials == null)
+            {
+                return Problem("Entity set 'DevXuongMocContext.Materials'  is null.");
+            }
+            var material = await _context.Materials.FindAsync(id);
+            if (material != null)
+            {
+                _context.Materials.Remove(material);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // POST: Admin/Materials/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    if (_context.Materials == null)
+        //    {
+        //        return Problem("Entity set 'DevXuongMocContext.Materials'  is null.");
+        //    }
+        //    var material = await _context.Materials.FindAsync(id);
+        //    if (material != null)
+        //    {
+        //        _context.Materials.Remove(material);
+        //    }
+            
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
+
+        private bool MaterialExists(int id)
+        {
+          return (_context.Materials?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
